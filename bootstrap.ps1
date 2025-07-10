155c813c140e858133a68e494b325bcbe548dc83
cuinixam <22344617+cuinixam@users.noreply.github.com>
2025-07-10 14:59:16 +0200

    fix: obsolete typer dependency

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -5,7 +5,7 @@
 
 function Invoke-Bootstrap {
     # Download bootstrap scripts from external repository
-    Invoke-RestMethod https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.15.1/install.ps1 | Invoke-Expression
+    Invoke-RestMethod https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.17.1/install.ps1 | Invoke-Expression
     # Execute bootstrap script
     . .\.bootstrap\bootstrap.ps1
 }
