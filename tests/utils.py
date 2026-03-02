2bf13bdc70c57302d8ce90edb7ea083016fdb5db
code-steadfast <akshitkobagapu+github@gmail.com>
2026-03-02 12:58:20 +0100

    chore: use pypeline and switch to uv

--- tests/utils.py
+++ tests/utils.py
@@ -1,20 +1,20 @@
-from typing import Any, List, Type, TypeVar
+from typing import Any, TypeVar
 
 T = TypeVar("T")
 
 
-def find_elements_of_type(elements: List[Any], element_type: Type[T]) -> List[T]:
+def find_elements_of_type(elements: list[Any], element_type: type[T]) -> list[T]:
     return [elem for elem in elements if isinstance(elem, element_type)]
 
 
-def assert_element_of_type(elements: List[Any], element_type: Type[T]) -> T:
+def assert_element_of_type(elements: list[Any], element_type: type[T]) -> T:
     elements = find_elements_of_type(elements, element_type)
     assert elements, f"No element of type {element_type.__name__} found"
     assert len(elements) == 1, f"More than one element of type {element_type.__name__} found"
     return elements[0]
 
 
-def assert_elements_of_type(elements: List[Any], element_type: Type[T], count: int) -> List[T]:
+def assert_elements_of_type(elements: list[Any], element_type: type[T], count: int) -> list[T]:
     elements = find_elements_of_type(elements, element_type)
     assert len(elements) == count, f"Expected {count} elements of type {element_type.__name__}, found {len(elements)}"
     return elements
